e035272a5556c4f69a65c225a7d1d0320e15e337
Lucas <lucaasferreirafl@gmail.com>
2022-06-15 22:00:19 -0300

    feat: handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'dotenv'
-import express from 'express'
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
 
 import { routes } from './routes'
 
@@ -8,5 +9,20 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+
+})
 
 app.listen(3000, () => console.log('Server is running on PORT 3333'))
